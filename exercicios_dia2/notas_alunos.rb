daf004158a645b71f85d90e0b085f2328c4e389a
Rolf Zambon <led479.rs@gmail.com>
2018-08-31 17:58:09 -0300

    Hash de alunos com notas

--- exercicios_dia2/notas_alunos.rb
+++ exercicios_dia2/notas_alunos.rb
@@ -1,2 +1,22 @@
 class Notas_alunos
+  attr_accessor :notas
+
+  def initialize
+    @notas = Hash.new
+  end
+
+  def adicionar_aluno nome, nota
+    @notas[nome] = nota
+  end
+
+  def imprimir_lista
+    @notas.each do |nome, nota|
+      puts "Aluno #{nome} possui a nota #{nota}"
+    end
+  end
 end
+
+db = Notas_alunos.new
+db.adicionar_aluno "Jonas", 10
+db.adicionar_aluno "Rolf", 12
+db.imprimir_lista
