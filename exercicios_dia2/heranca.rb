9ebfecdf6bf71f2409f2ec74f3bbb066339ffadb
Rolf Zambon <led479.rs@gmail.com>
2018-08-31 16:53:15 -0300

    Exercicios de heranca feitos

--- exercicios_dia2/heranca.rb
+++ exercicios_dia2/heranca.rb
@@ -1,13 +1,31 @@
 class Pessoa
   attr_accessor :nome, :cpf
 
-  def initialize(nome, cpf)
+  def initialize nome, cpf
     @cpf = cpf
     @nome = nome
   end
 
   def exibe_pessoa
-    puts "Eu sou o #{@nome}, com o cpf #{@cpf}."
+    print "Eu sou o #{@nome}, com o cpf #{@cpf}"
   end
 end
 
+
+class Empregado < Pessoa
+  attr_accessor :salario
+
+  def initialize nome, cpf, salario
+    super(nome, cpf)
+    @salario = salario
+  end
+
+  def exibe_pessoa
+    super
+    puts " e meu salário é de R$#{@salario}."
+
+  end
+end
+
+empregado = Empregado.new "Jonas", 1212, 10000
+empregado.exibe_pessoa
